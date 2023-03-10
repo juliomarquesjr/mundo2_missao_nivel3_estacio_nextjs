c9735a9673676fd3e6f4b0ad60f8ab1fdba0fdd8
Julio Marques <juliomarquesjr@yahoo.com.br>
2023-03-10 09:50:25 -0300

    Tratamento de erros

--- src/pages/api/livros/index.ts
+++ src/pages/api/livros/index.ts
@@ -4,6 +4,5 @@ import { ControleLivro } from "@/classes/controle/ControleLivros";
 export const livros = new ControleLivro();
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { codigo } = req.query;
   res.status(200).json(livros.obterLivros());
 };
