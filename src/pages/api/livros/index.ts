72d3f361671f319788a81b7e04dafb95cb39c642
Julio Marques <juliomarquesjr@yahoo.com.br>
2023-03-10 11:58:31 -0300

    Add POST e DELETE a API

--- src/pages/api/livros/index.ts
+++ src/pages/api/livros/index.ts
@@ -1,8 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ControleLivro } from "@/classes/controle/ControleLivros";
+import { Livro } from "@/classes/modelo/Livro";
 
 export const livros = new ControleLivro();
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(livros.obterLivros());
+  const requestMethod = req.method;
+  const { livro, codigo_livro } = JSON.parse(req.body);
+
+  switch (requestMethod) {
+    case "POST":
+      const novoLivro = new Livro(
+        livro.codigo,
+        livro.editora,
+        livro.titulo,
+        livro.resumo,
+        livro.autores
+      );
+      livros.incluir(novoLivro);
+      res.status(200).json(livros.obterLivros());
+      break;
+
+    case "DELETE":
+      livros.excluir(Number(codigo_livro));
+      res.status(200).json(livros.obterLivros());
+      break;
+
+    default:
+      res.status(200).json(livros.obterLivros());
+      break;
+  }
 };
